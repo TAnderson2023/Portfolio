c6fec9d910da510453607ab85e68350fd2061786
Tristan Anderson <56132462+TAnderson2023@users.noreply.github.com>
2024-05-21 13:54:56 -0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-import "./App.css";
+import Button from "./components/Button";
 
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+      <button />
+    </>
+  );
+};
+
+export default App;

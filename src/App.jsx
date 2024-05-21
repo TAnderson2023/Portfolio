1590ef23d16b3f8b7d4b10196e93594fe8ece40b
Tristan Anderson <56132462+TAnderson2023@users.noreply.github.com>
2024-05-21 14:10:20 -0500

    deploy time test

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <div className="pt-[4.75rem] pl-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Button className="mt-10" href="#login">
           Something
         </Button>
       </div>
-      <button />
     </>
   );
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,13 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button`;
+
+  const renderButton = () => (
+    <button className={classes}>
+      <span>{children}</span>
+    </button>
+  );
+
+  return renderButton();
 };
 
 export default Button;
